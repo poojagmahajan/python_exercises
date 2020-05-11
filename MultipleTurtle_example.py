4584cb85c56e803c3f568598d0868b29bec5ac41
Pooja <mahajanpoojag@gmail.com>
2020-05-12 02:25:30 +0530

    Multiple turtle example

---
+++ MultipleTurtle_example.py
@@ -0,0 +1,19 @@
+# Multiple Turtle
+
+# You can make more than one turtle. Just give them different names! You can then make them do different things
+# or give them different colors.
+
+import turtle
+
+pooja=turtle.Turtle()
+gaurav=turtle.Turtle()
+
+pooja.color("Red")
+gaurav.color("Blue")
+
+pooja.forward(100)
+
+gaurav.left(40)
+gaurav.forward(100)
+
+turtle.done()
