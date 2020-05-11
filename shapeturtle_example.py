ee1662e7b11f05e324094b1215fe2736263bf176
Pooja <mahajanpoojag@gmail.com>
2020-05-12 02:32:46 +0530

    shape turtle  example

---
+++ shapeturtle_example.py
@@ -0,0 +1,20 @@
+
+#we can give different shapes to turtle
+
+import turtle
+
+pooja=turtle.Turtle()
+gaurav=turtle.Turtle()
+
+pooja.shape("circle")
+gaurav.shape("turtle")
+
+pooja.color("Green")
+gaurav.color("purple")
+
+pooja.forward(100)
+
+gaurav.left(50)
+gaurav.forward(100)
+
+turtle.done()
