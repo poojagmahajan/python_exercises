b1ae886b9f7630fab795f80c7709e7220d59c5cd
Pooja <mahajanpoojag@gmail.com>
2020-05-12 03:09:28 +0530

    Draw playground

---
+++ playground.py
@@ -0,0 +1,36 @@
+import turtle
+tina = turtle.Turtle()
+tina.shape("turtle")
+
+def draw_circle(turtle, color, size, x, y):
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x,y)
+    turtle.begin_fill()
+    turtle.circle(size)
+    turtle.end_fill()
+    turtle.pendown()
+
+
+def draw_square(turtle, color, size, x, y):
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.end_fill()
+
+
+draw_square(tina, "green", 100, 25, 0)
+draw_circle(tina, "blue", 50, 0, 0)
+draw_circle(tina, "yellow", 50, -25, 0)
