75e8f7f613ed3aab40609998748eec65c90f9f02
Pooja <mahajanpoojag@gmail.com>
2020-05-12 02:10:18 +0530

    All python exercises are here comment added

--- Turtle-exercise.py
+++ Turtle-exercise.py
@@ -1,6 +1,7 @@
 #make a Star
 # Link for Turtle examples to practice
 # https://michael0x2a.com/blog/turtle-examples
+#https://mahajanpoojag-7355.trinket.io/an-hour-of-python#/welcome-to-python/going-deeper-with-turtle
 
 import turtle
 
