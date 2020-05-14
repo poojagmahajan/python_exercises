10b50007cf8f084077661352c75fd50934e0e05a
Pooja <mahajanpoojag@gmail.com>
2020-05-14 22:44:40 +0530

    find index  in string xz

---
+++ practice programe/findindex_string.py
@@ -0,0 +1,14 @@
+# find index in given string  e.g. "aaabbbccc"
+
+def findIndex(s):
+
+    index1 = s.find("b")
+    index2 = s.find("ccc")
+
+    print(index1,index2)
+
+    #return(index1,index2)
+
+findIndex("aaabbbccc")
+
+#print(findIndex("aaabbbccc"))
