62c326c3e8e89dd92ecd522862a8e92c269bb7c5
Pooja <mahajanpoojag@gmail.com>
2020-05-14 23:34:24 +0530

    chang case of string
    use lower(),upper()

---
+++ practice programe/string_case.py
@@ -0,0 +1,10 @@
+# change case of given string
+
+def changeCase(s) :
+    result1 = s.upper()
+    result2 = result1.lower()
+    result3 = s.lower()
+    return(result1,result2,result3)
+
+print(changeCase("Pooja Mahajan"))
+
