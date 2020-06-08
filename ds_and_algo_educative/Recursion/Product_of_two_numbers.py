2129adb90dcb6af57788bd192b7568f73085afe7
Pooja <mahajanpoojag@gmail.com>
2020-06-09 02:58:38 +0530

    Problems on Recursion are in this directory

---
+++ ds_and_algo_educative/Recursion/Product_of_two_numbers.py
@@ -0,0 +1,21 @@
+
+
+##  Hint ;  5 * 3 = 5 + 5 + 5 = 15
+
+##  https://www.youtube.com/watch?v=VapDVrsERsA
+
+def recursive_multiply(x, y):
+
+    # This cuts down on the total number of
+    # recursive calls:
+    if x < y:
+        return recursive_multiply(y, x)
+    if y == 0:
+        return 0
+    return x + recursive_multiply(x, y-1)
+
+x = 500
+y = 2000
+
+print(x * y)
+print(recursive_multiply(x, y))
